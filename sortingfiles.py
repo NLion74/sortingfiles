513a50a11b762666fe55c902bfa716398a9ce994
NLion74 <97677226+NLion74@users.noreply.github.com>
2022-09-01 19:43:10 +0200

    Add files via upload

--- sortingfiles.py
+++ sortingfiles.py
@@ -1,11 +1,10 @@
 import os
 import shutil
 
-path = 'Drive:/path/to/your/files'
-
+path = 'Drive:/path/to/my/files'
 
 documents = ['zip', 'rar', '7z', 'gz', 'txt']
-documentspath = 'Drive:path/to/your/documents'
+documentspath = 'Drive:/path/to/my/documents'
 
 if os.path.exists(documentspath):
     pass
@@ -19,7 +18,7 @@ else:
 os.system(documentspath[0:2] + " && cd " + documentspath)
 
 office = ['doc', 'xls', 'ppt', 'odt', 'ods', 'pdf', 'docx', 'pptx', 'xlsx']
-officepath = 'Drive:path/to/your/office'
+officepath = 'Drive:/path/to/my/office'
 
 if os.path.exists(officepath):
     pass
@@ -33,7 +32,7 @@ else:
 os.system(officepath[0:2] + " && cd " + officepath)
 
 images = ['jpg', 'jpeg', 'png', 'raw', 'gif', 'bmp', 'tif']
-imagepath = 'Drive:path/to/your/images'
+imagepath = 'Drive:/path/to/my/images'
 
 if os.path.exists(imagepath):
     pass
@@ -47,7 +46,7 @@ else:
 os.system(imagepath[0:2] + " && cd " + imagepath)
 
 videos = ['avi', 'mkv', 'mov', 'mpg', 'mp4', 'flv', 'wmv']
-videopath = 'Drive:path/to/your/videos'
+videopath = 'Drive:/path/to/my/videos'
 
 if os.path.exists(videopath):
     pass
@@ -61,7 +60,7 @@ else:
 os.system(videopath[0:2] + " && cd " + videopath)
 
 music = ['mp3', 'wma', 'ogg', 'wav', 'aac', 'm4a','mka']
-musicpath = 'Drive:path/to/your/music'
+musicpath = 'Drive:/path/to/my/music'
 
 if os.path.exists(musicpath):
     pass
@@ -92,7 +91,7 @@ for file_ in list_:
     if ext == '':
         continue
 
-    if ext == listext(list=documents, ext=ext):
+    elif ext == listext(list=documents, ext=ext):
         if os.path.exists(documentspath):
             shutil.move(path + '/' + file_, documentspath + '/' + file_)
         else:
